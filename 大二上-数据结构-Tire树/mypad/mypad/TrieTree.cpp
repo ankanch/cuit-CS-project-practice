4b3a07ce68ff396876e4eff57c97c05317c169af
Kanch <kanchisme@gmail.com>
2016-12-12 22:08:08 +0800

    开始完成Trie类的相关辅助辅助函数（1）

--- 大二上-数据结构-Tire树/mypad/mypad/TrieTree.cpp
+++ 大二上-数据结构-Tire树/mypad/mypad/TrieTree.cpp
@@ -4,19 +4,79 @@
 
 CTrieTree::CTrieTree()
 {
+	proot = new WORDNODE;
+	proot->ch = '*';
+	proot->pdata = nullptr;
+	proot->pnextlist = nullptr;
+	proot->size_nextlist = 0;
+	levelnodes_count_list = new int[BUFFER_LEVEL_COUNT];  //初始化层节点数数组
+	level_count = 1;
+	nodes_count = 0;
+	diff_words_count = 0;
+	size_LLCL = 0;
+}
+
+CTrieTree::~CTrieTree()
+{
+	delete proot;
+	delete[] levelnodes_count_list;
 }
 
 const bool CTrieTree::Insert(CString word)
 {
+
 	return false;
 }
 
-const bool CTrieTree::Find(CString word)
+const bool CTrieTree::Search(CString word)
 {
 	return false;
 }
 
-
-CTrieTree::~CTrieTree()
+const PROOT CTrieTree::GetRoot()
 {
+	return proot;
 }
+
+const int CTrieTree::GetNodesCount()
+{
+	return nodes_count;
+}
+
+const int CTrieTree::GetLevelCount()
+{
+	return level_count;
+}
+
+const bool CTrieTree::AppendMemoryForLLCL()
+{
+	try {
+		int *buf = new int[size_LLCL + BUFFER_INCREMENT_LEVEL_COUNT];
+		for (int i = 0; i < size_LLCL; i++)
+		{
+			buf[i] = levelnodes_count_list[i];
+		}
+		delete[] levelnodes_count_list;
+		levelnodes_count_list = buf;
+	}catch(...){
+		//出现未知错误
+		return false;
+	}
+	return true;
+}
+
+const int CTrieTree::GetLLCLSize()
+{
+	return size_LLCL;
+}
+
+const int CTrieTree::SearchForAlphabetIndex(const char ch, const WORDNODE & node)
+{
+	for (int i = 0; i < node.size_nextlist; i++)
+	{
+		node.pnextlist[i]->ch == ch;
+		return i;
+	}
+	return -1;
+}
+
--- 大二上-数据结构-Tire树/mypad/mypad/TrieTree.h
+++ 大二上-数据结构-Tire树/mypad/mypad/TrieTree.h
@@ -1,26 +1,43 @@
 #pragma once
 
+#define BUFFER_LEVEL_COUNT 20   //每一层的节点数数组的初始长度（假设会有20层）
+#define BUFFER_INCREMENT_LEVEL_COUNT 10  //层节点数数组的递增量
+
 typedef struct leafdata {
-	int word_count;
-	int word_length;
+	int word_count;			//该单词出现的次数
+	int word_length;		//该单词的长度
 }LEAFDATA,*PLEAFDATA;
 
 typedef struct TNode{
-	char ch;
-	struct TNode *pnextlist;
-	PLEAFDATA pdata;
+	char ch;				//储存的字符
+	int size_nextlist;		//节点索引大小
+	struct TNode* *pnextlist;	//节点索引指针数组
+	PLEAFDATA pdata;			//如果是叶子节点，这里则应该包含数据
 }WORDNODE,*PWORDNODE;
+
+typedef PWORDNODE PROOT;
 
 class CTrieTree
 {
 public:
 	CTrieTree();
-	const bool Insert(CString word);  //将一个单词插入Trie树
-	const bool Find(CString word);		//查找一个单词是否已经在Trie树中
 	~CTrieTree();
+	const bool Insert(CString word);  //将一个单词插入Trie树
+	const bool Search(CString word);		//查找一个单词是否已经在Trie树中
+	const PROOT GetRoot();  //返回树根
+	const int GetNodesCount();  //返回节点数
+	const int GetLevelCount();  //树的深度（包括树根）
 private:
-	PWORDNODE proot;
-	int diff_words_count;
-	int nodes_count;
+	const bool AppendMemoryForLLCL();  //该函数用来向levelnodes_count_list追加空间
+	const int GetLLCLSize();  //获取levelnodes_count_list的大小
+	const int SearchForAlphabetIndex(const char ch,const WORDNODE & node); //该函数用来查找数据为ch的节点在当前node中的pnexlist索引,未找到：-1
+	
+	PROOT proot;		//指向树根
+	int diff_words_count; //记录不同单词个数
+	int nodes_count;	//记录总的节点数目
+	int level_count;   //记录树的深度
+
+	int size_LLCL;   //记录levelnodes_count_list的大小
+	int *levelnodes_count_list;	//记录树每一层的节点数
 };
 
